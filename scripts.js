abc3e39799def3df50a39418310114089842a7df
Nkaka Yves <nkakayves515@gmail.com>
2020-10-11 08:56:57 +0200

    adding menu and what we do section

---
+++ scripts.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+    $(".hid1").click(function() {
+      $(".dis1").toggle();
+      $(".hid1").toggle();
+    });
+    $(".hid2").click(function() {
+      $(".dis2").toggle();
+      $(".hid2").toggle();
+    });
+    $(".hid3").click(function() {
+      $(".dis3").toggle();
+      $(".hid3").toggle();
+    });
+  });
+  
